02dd2101f3ad30e8346fd31db4270b5ef26f95e4
Ashish Bhatia <ashishb@axsource.com>
2026-05-12 19:24:03 +0000

    Fix Swagger package compatibility

---
+++ src/Infrastructure/DependencyInjection/DependencyInjection.cs
@@ -0,0 +1,12 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure.DependencyInjection;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services)
+    {
+        return services;
+    }
+}
